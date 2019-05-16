b3fa223cf7e58f4e80657368d1620c7df9275e11
Evan Bacon <baconbrix@gmail.com>
2019-05-15 20:16:21 -0700

    Fixed image resizing

--- components/ItemImage.js
+++ components/ItemImage.js
@@ -3,26 +3,35 @@ import { Image, Dimensions } from 'react-native';
 
 class ItemImage extends React.PureComponent {
   state = {
-    width: undefined,
-    height: undefined,
+    imgWidth: undefined,
+    imgHeight: undefined,
     isLoaded: false,
   };
   componentDidMount() {
     if (!this.props.imageWidth) {
-      Image.getSize(this.props.source.uri, (width, height) => {
-        this.setState({ width, height, isLoaded: true });
+      Image.getSize(this.props.source.uri, (imgWidth, imgHeight) => {
+        this.setState({ imgWidth, imgHeight, isLoaded: true });
       });
     }
   }
+  resize = ({
+    nativeEvent: {
+      layout: { width, height },
+    },
+  }) => {
+    this.setState({ width, height, isLoaded: true });
+  };
+
   render() {
     const { imageWidth, imageHeight, source } = this.props;
-    const fullWidth = Dimensions.get('window').width;
-    const imgW = imageWidth || this.state.width || fullWidth;
-    const imgH = imageHeight || this.state.height || imgW;
+    const fullWidth = this.state.width || Dimensions.get('window').width;
+    const imgW = imageWidth || this.state.imgWidth || fullWidth;
+    const imgH = imageHeight || this.state.imgHeight || imgW;
     const aspectRatio = imgH / imgW;
     const adjustedHeight = fullWidth * aspectRatio;
     return (
       <Image
+        onLayout={this.resize}
         style={{
           resizeMode: 'contain',
           height: adjustedHeight,
