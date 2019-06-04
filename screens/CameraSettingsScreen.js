e2aaa3e430f92872e02ea020528b31326ecd9f85
Evan Bacon <baconbrix@gmail.com>
2019-06-03 20:04:06 -0700

    Update CameraSettingsScreen.js

--- screens/CameraSettingsScreen.js
+++ screens/CameraSettingsScreen.js
@@ -6,7 +6,13 @@ import InstaIcon from '../components/InstaIcon';
 export default class CameraSettingsScreen extends React.Component {
   static navigationOptions = navigation => ({
     title: 'Story Controls',
-    renderLeft: () => <InstaIcon name="cancel" color="black" onPress={() => navigation.goBack()} />,
+    renderLeft: () => (
+      <InstaIcon
+        name="cancel"
+        color="black"
+        onPress={() => navigation.goBack()}
+      />
+    ),
   });
   render() {
     return <View />;
